f375047d963c8d27ede615744c78834eed8c96dd
unknown <ronan.diot@aiyo.fr>
2019-10-29 18:58:06 +0100

    add exceldna interface, add choleskydecomposition

---
+++ PathDependentOption/PathDependentOption/ExcelInterface/ExcelConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PathDependentOption.Mathematics;
+
+namespace PathDependentOption.ExcelInterface
+{
+    public static class ExcelConverter
+    {
+        public static Matrix CreateMatrix(object[,] tab)
+        {
+            var M = new Matrix(tab.GetLength(0), tab.GetLength(1));
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    M[i, j] = (double)tab[i, j];
+                }
+            }
+            return M;
+        }
+
+        public static double[,] ConvertMatrix(Matrix M)
+        {
+            var tab = new double[M.Get_Rows(), M.Get_Cols()];
+            for (int i = 0; i < M.Get_Rows(); i++)
+            {
+                for (int j = 0; j < M.Get_Cols(); j++)
+                {
+                    tab[i, j] = M[i, j];
+                }
+            }
+            return tab;
+        }
+    }
+}
